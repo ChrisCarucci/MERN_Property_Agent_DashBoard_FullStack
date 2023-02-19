eb86bb566611af5de20d15efbb6e57ea75861014
ChrisCarucci <Chris.w.carucci@gmail.com>
2023-02-19 17:21:03 -0330

    Home Page Setupd + Chart Config

--- refine_dashboard/client/src/pages/home.tsx
+++ refine_dashboard/client/src/pages/home.tsx
@@ -1,10 +1,57 @@
 import React from 'react';
 import { useList } from '@pankod/refine-core';
+import { Box, Stack, Typography } from '@pankod/refine-mui';
+
+import {
+  PieChart,
+  PropertyReferrals,
+  TotalRevenue,
+  PropertyCard,
+} from 'components'
 
 
 const home = () => {
   return (
-    <div>home</div>
+    <Box>
+      <Typography fontSize={25} fontWeight={700} color="#111420">
+        Dashboard
+      </Typography>
+
+      <Box mt="20px" display="flex" 
+      flexWrap="wrap" gap={4}>
+        <PieChart
+          title="Properties for Sale"
+          value={684}
+          series={[75,25]}
+          colors={['#475be8', '#e4e8ef']}
+        />
+        <PieChart
+          title="Properties for Rent"
+          value={550}
+          series={[60,40]}
+          colors={['#475be8', '#e4b8ef']}
+        />
+        <PieChart
+          title="Total Customers"
+          value={684}
+          series={[75,25]}
+          colors={['#275be8', '#c4e8ef']}
+        />
+        <PieChart
+          title="Properties for Cities"
+          value={684}
+          series={[75,25]}
+          colors={['#475be8', '#e4e8ef']}
+        />
+      </Box>
+
+      <Stack mt="25px" width="100%"
+      direction={{ xs: 'column', lg: 'row'}}>
+        <TotalRevenue />
+        <PropertyReferrals />
+      </Stack>
+
+    </Box>
   )
 }
 
