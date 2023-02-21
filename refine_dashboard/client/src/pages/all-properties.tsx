79dd52fac1d8cba5bcc8c6956da7be6e615b59d0
ChrisCarucci <Chris.w.carucci@gmail.com>
2023-02-20 21:18:10 -0330

    Bug Fix

--- refine_dashboard/client/src/pages/all-properties.tsx
+++ refine_dashboard/client/src/pages/all-properties.tsx
@@ -38,113 +38,113 @@ const AllProperties = () => {
 
     const currentFilterValues = useMemo(() => {
         const logicalFilters = filters.flatMap((item) =>
-            "field" in item ? item : [],
+          "field" in item ? item : [],
         );
 
-        return {
-            title:
-                logicalFilters.find((item) => item.field === "title")?.value ||
-                "",
-            propertyType:
-                logicalFilters.find((item) => item.field === "propertyType")
-                    ?.value || "",
-        };
-    }, [filters]);
+    return {
+      title:
+        logicalFilters.find((item) => item.field === "title")?.value ||
+        "",
+      propertyType:
+        logicalFilters.find((item) => item.field === "propertyType")
+        ?.value || "",
+    };
+  }, [filters]);
 
     if (isLoading) return <Typography>Loading...</Typography>;
     if (isError) return <Typography>Error...</Typography>;
 
     return (
         <Box>
-            <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
-                <Stack direction="column" width="100%">
-                    <Typography fontSize={25} fontWeight={700} color="#11142d">
-                      {!allProperties.length
-                        ? "There are no properties"
-                        : "All Properties"}
-                    </Typography>
+          <Box mt="20px" sx={{ display: "flex", flexWrap: "wrap", gap: 3 }}>
+            <Stack direction="column" width="100%">
+                <Typography fontSize={25} fontWeight={700} color="#11142d">
+                  {!allProperties.length
+                    ? "There are no properties"
+                    : "All Properties"}
+                </Typography>
+                  <Box
+                    mb={2}
+                    mt={3}
+                    display="flex"
+                    width="84%"
+                    justifyContent="space-between"
+                    flexWrap="wrap"
+                  >
                     <Box
-                      mb={2}
-                      mt={3}
                       display="flex"
-                      width="84%"
-                      justifyContent="space-between"
+                      gap={2}
                       flexWrap="wrap"
+                      mb={{ xs: "20px", sm: 0 }}
                     >
-                      <Box
-                        display="flex"
-                        gap={2}
-                        flexWrap="wrap"
-                        mb={{ xs: "20px", sm: 0 }}
-                      >
-                      <CustomButton
-                          title={`Sort price ${
-                          currentPrice === "asc" ? "↑" : "↓"
-                          }`}
-                          handleClick={() => toggleSort("price")}
-                          backgroundColor="#475be8"
-                          color="#fcfcfc"
-                      />
-                      <TextField
-                          variant="outlined"
-                          color="info"
-                          placeholder="Search by title"
-                          value={currentFilterValues.title}
-                          onChange={(e) => {
-                            setFilters([
-                              {
-                                field: "title",
-                                operator: "contains",
-                                value: e.currentTarget.value
-                                  ? e.currentTarget.value
-                                  : undefined,
-                              },
-                            ]);
-                          }}
-                      />
-                      <Select
+                    <CustomButton
+                        title={`Sort price ${
+                        currentPrice === "asc" ? "↑" : "↓"
+                        }`}
+                        handleClick={() => toggleSort("price")}
+                        backgroundColor="#475be8"
+                        color="#fcfcfc"
+                    />
+                    <TextField
                         variant="outlined"
                         color="info"
-                        displayEmpty
-                        required
-                        inputProps={{ "aria-label": "Without label" }}
-                        defaultValue=""
-                        value={currentFilterValues.propertyType}
+                        placeholder="Search by title"
+                        value={currentFilterValues.title}
                         onChange={(e) => {
-                          setFilters(
-                              [
-                                {
-                                  field: "propertyType",
-                                  operator: "eq",
-                                  value: e.target.value,
-                                },
-                              ],
-                            "replace",
-                          );
+                          setFilters([
+                            {
+                              field: "title",
+                              operator: "contains",
+                              value: e.currentTarget.value
+                                ? e.currentTarget.value
+                                : undefined,
+                            },
+                          ]);
                         }}
+                    />
+                    <Select
+                      variant="outlined"
+                      color="info"
+                      displayEmpty
+                      required
+                      inputProps={{ "aria-label": "Without label" }}
+                      defaultValue=""
+                      value={currentFilterValues.propertyType}
+                      onChange={(e) => {
+                        setFilters(
+                            [
+                              {
+                                field: "propertyType",
+                                operator: "eq",
+                                value: e.target.value,
+                              },
+                            ],
+                          "replace",
+                        );
+                      }}
+                    >
+                      <MenuItem value="">All</MenuItem>
+                      {[
+                          "Apartment",
+                          "Villa",
+                          "Farmhouse",
+                          "Condos",
+                          "Townhouse",
+                          "Duplex",
+                          "Studio",
+                          "Chalet",
+                      ].map((type) => (
+                        <MenuItem
+                          key={type}
+                          value={type.toLowerCase()}
                       >
-                        <MenuItem value="">All</MenuItem>
-                        {[
-                            "Apartment",
-                            "Villa",
-                            "Farmhouse",
-                            "Condos",
-                            "Townhouse",
-                            "Duplex",
-                            "Studio",
-                            "Chalet",
-                        ].map((type) => (
-                          <MenuItem
-                            key={type}
-                            value={type.toLowerCase()}
-                        >
-                          {type}
-                          </MenuItem>
-                        ))}
-                    </Select>
-                  </Box>
+                        {type}
+                        </MenuItem>
+                      ))}
+                  </Select>
                 </Box>
-            </Stack>
+              </Box>
+          </Stack>
         </Box>
           <Stack
             direction="row"
